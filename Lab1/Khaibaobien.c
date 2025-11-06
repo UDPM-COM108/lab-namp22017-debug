ab93666b27cbcebfc698f6672072e353c004df02
PHAM PHUONG NAM <namp22017@gmail.com>
2025-11-07 00:37:13 +0700

    nop bai lab 1

--- Lab1/Khaibaobien.c
+++ Lab1/Khaibaobien.c
@@ -3,7 +3,7 @@
 int main (){
     int a, b;
     printf("Hello, chào các bạn");
-    printf("\nnhập vao a và b: ");
+    printf("\nnhap vao a và b: ");
     scanf("%d %d", &a,&b);
      printf("Bạn vừa nhập a = %d, b = %d\n", a, b);
     return 0;
